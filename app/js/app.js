c88e541c36049c312f3d30ec11ef5148757a9775
ASHISH KARKI <ashishkarki@users.noreply.github.com>
2015-04-17 14:41:54 -0400

    Almost done with the other directives- part 2 of the tutorial.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // this is definition of a module. [] means this module does not depend on anything else
-var eventsApp = angular.module('eventsApp', []);
+var eventsApp = angular.module('eventsApp', ['ngSanitize']);
--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,11 @@
 
 eventsApp.controller('EventController',
     function EventController($scope){
+        $scope.snippet = '<span style="color: red">Hi There</span>';
+        $scope.boolValue = false;
+        $scope.mystyle = {color: 'red'};
+        $scope.myclass = "blue";
+        $scope.buttonDisabled = true;
         // event exists within the 'scope' of EventController
         $scope.event = {
             name: 'Angular Boot Camp',
@@ -11,7 +16,42 @@ eventsApp.controller('EventController',
                 address: 'My company inc',
                 city: 'My city name',
                 province: 'My state name'
-            }
-        }
+            },
+            imageUrl: '/img/angularjs-image.jpg',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creator: 'Ashish Karki',
+                    duration: '1 hour',
+                    level: 'Advanced',
+                    abstract: 'Learn goodies about directives',
+                    upVoteCount: 2
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creator: 'SomeName Guy',
+                    duration: '2 hour',
+                    level: 'Intermediate',
+                    abstract: 'Take a closer look at scopes',
+                    upVoteCount: 3
+                },
+                {
+                    name: 'Well Behaved Controller',
+                    creator: 'AnotherPerson Dude',
+                    duration: '1/2 hour',
+                    level: 'Advanced',
+                    abstract: 'Controllers are the beginning of everything. So start knowing them properly.',
+                    upVoteCount: 20
+                }
+            ]
+        } // end of $scope.event
+
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        };
     }
 );
